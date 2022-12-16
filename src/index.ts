9e52309f6091503e8a2417494412d76bc4fb7a8c
Elton Kornmann <elton.kornmann@gmail.com>
2022-12-16 13:28:10 +0100

    Delete the console.log statement

--- src/index.ts
+++ src/index.ts
@@ -2,7 +2,6 @@ function greet(name: string, birthYear: number) : string {
   const age = new Date().getFullYear() - birthYear;
   return `Hello ${name}, you are ${age} years old`;
 }
-console.log(greet('My name', 1999));
 export {
   greet,
 };
